0e1789ad0949574bf12473ff77fe7ab32194c807
Kytsu01 <nicolasmfrancisco@gmail.com>
2025-08-02 12:41:04 -0300

    Initial Structure: Projeto Razor Pages com EF Core

---
+++ RazorPagesMovies/Data/RazorPagesMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovies.Movies;
+
+namespace RazorPagesMovies.Data
+{
+    public class RazorPagesMoviesContext : DbContext
+    {
+        public RazorPagesMoviesContext (DbContextOptions<RazorPagesMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovies.Movies.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ RazorPagesMovies/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovies.Data;
+using RazorPagesMovies.Movies;
+
+namespace RazorPagesMovies.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorPagesMovies.Data.RazorPagesMoviesContext _context;
+
+        public DetailsModel(RazorPagesMovies.Data.RazorPagesMoviesContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (movie is not null)
+            {
+                Movie = movie;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+    }
+}
